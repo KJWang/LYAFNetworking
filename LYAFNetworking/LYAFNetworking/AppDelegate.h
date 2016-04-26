1501acc3c958843c5a54dd263a1e46b6f2b43c3a
WangKJ <wangjie_app@163.com>
2016-04-26 18:33:16 +0800

    What summary ?
    Create Project，Import AFNetworking

---
+++ LYAFNetworking/LYAFNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYAFNetworking
+//
+//  Created by Wang on 16/4/26.
+//  Copyright © 2016年 云客. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYAFNetworking/LYAFNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYAFNetworking
+//
+//  Created by Wang on 16/4/26.
+//  Copyright © 2016年 云客. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
